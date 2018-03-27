415b7b6631134d77f0512a39cef6900873785b63
Mike Larocca <michaellarocca90@gmail.com>
2018-03-27 16:40:59 -0700

    added a basic registration form and some tests for it.  Got stuck on the test for submitting a form, moving on to testing the logging of a state update on form change.

--- travel_app_frontend/src/__tests__/Register.js
+++ travel_app_frontend/src/__tests__/Register.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Register from '../pages/Register';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
@@ -11,6 +11,45 @@ it('renders without crashing', ()=>{
   const div = document.createElement('div')
   ReactDOM.render(<Register />, div)
 })
+
+it('has an first name input', ()=>{
+  const component = mount(<Register />);
+  expect(component.find('#first_name').text()).toBe('First Name')
+})
+
+it('has an last name input', ()=>{
+  const component = mount(<Register />);
+  expect(component.find('#last_name').text()).toBe('Last Name')
+})
+
+it('has an email input', ()=>{
+  const component = mount(<Register />);
+  expect(component.find('label#email').text()).toBe('Email')
+})
+
+it('has an password input', ()=>{
+  const component = mount(<Register />);
+  expect(component.find('label#password').text()).toBe('Password')
+})
+
+it('has an password confirmation input', ()=>{
+  const component = mount(<Register />);
+  expect(component.find('label#password_confirmation').text()).toBe('Password Confirm')
+})
+
+it('has a submit button', ()=>{
+    const mockSubmitHandler = jest.fn()
+    const component = mount (<Register onSubmit={mockSubmitHandler}/>)
+    component.find('input[name="name"]').simulate('change', {target: {value: 'Paws', name: 'name'}})
+})
+
+// it('has a submit button', ()=>{
+//   const mockSubmitHandler = jest.fn()
+//   const component = mount(<Register />);
+//   component.find('form').simulate('submit')
+//   // console.log(mockSubmitHandler.mock)
+//   expect(mockSubmitHandler).toBeCalled();
+// })
 
 // it('', ()=>{
 //
--- travel_app_frontend/src/pages/Register.js
+++ travel_app_frontend/src/pages/Register.js
@@ -2,9 +2,84 @@ import React, { Component } from 'react';
 
 
 class Register extends Component {
+    constructor(){
+      super()
+      this.state = {
+        form: {
+          first_name: '',
+          last_name: '',
+          city: '',
+          state: '',
+          email: '',
+          password: '',
+          password_confirmation: '',
+          //TODO profile pic avatar
+        }
+      }
+    }
+
+handleChange(e){
+  
+}
+
+handleSubmit(event){
+
+  event.preventDefault()
+
+  return("Form was submitted", this.state)
+}
+
   render() {
     return(
-      <div>Something</div>
+      <div>
+        <form
+          onSubmit={this.handleSubmit.bind(this)}
+        >
+          <label id='first_name'>First Name</label>
+          <input
+            placeholder="First Name"
+            name='first_name'
+            value= {this.state.form.first_name}
+            onChange={this.handleChange.bind(this)}
+            type="text"
+          />
+          <label id='last_name'>Last Name</label>
+          <input
+            placeholder="Last Name"
+            type="text"
+          />
+          <label id='city'>City</label>
+          <input
+            placeholder="City"
+            type="text"
+          />
+          <label id='state'>State</label>
+          <input
+            placeholder="State"
+            type="text"
+          />
+          <label id='email'>Email</label>
+          <input
+            placeholder="email"
+            type="email"
+          />
+          <label id='password'>Password</label>
+          <input
+            placeholder="password"
+            type="password"
+          />
+          <label id='password_confirmation'>Password Confirm</label>
+          <input
+            placeholder="Confirm Password"
+            type="password"
+          />
+          <input
+            className='form-submit'
+            type="submit"
+            value='Submit'
+          />
+        </form>
+      </div>
     );
 
   }
