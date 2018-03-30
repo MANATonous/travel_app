5546fb1f3a758a699e25d4128db4b50412aa4275
learn <theresamagner@gmail.com>
2018-03-30 11:38:59 -0700

    finished join trip function, needs testing

--- travel_app_backend/app/controllers/trips_controller.rb
+++ travel_app_backend/app/controllers/trips_controller.rb
@@ -9,6 +9,21 @@ class TripsController < ApplicationController
     render json: trip
   end
 
+  def join
+    user = User.find_by_id(params[:user_id])
+    code = params[:code].to_s
+
+    if trip = Trip.find_by_rand_code(code)
+      ut = UserTrip.new()
+      ut.user_id = user.id
+      ut.trip_id = trip.id
+      ut.save!
+      render json: trip, status: 201
+    else
+      render json: {errors: user.errors}, status: 404
+    end
+  end
+
   private
   def trip_params
     params.require(:trip).permit(:title, :city, :state, :country, :start_date, :end_date, :description, :link, :rand_code, :user_id)
--- travel_app_frontend/src/pages/JoinTrip.js
+++ travel_app_frontend/src/pages/JoinTrip.js
@@ -1,29 +1,63 @@
 import React, { Component } from 'react';
 import {Row, Col, Form} from 'react-bootstrap';
+import AuthService from '../services/AuthService';
 
 class JoinTrip extends Component {
   constructor(props){
     super(props)
+    this.Auth = new AuthService()
     this.state = {
       apiUrl: 'http://localhost:3000',
-      code: ''
+      info: {
+        code: '',
+        user_id: ''
+      }
     }
   }
 
   handleChange(e){
-    let codeVal = this.state.code
-    codeVal = e.target.value
-    this.setState({code: codeVal})
-    console.log(this.state.code);
+    const { info } = this.state
+    info[e.target.name] = e.target.value
+    info.user_id = this.Auth.getUserId()
+    this.setState({ info })
+  }
+
+  joinTripSubmit(event){
+    //when a submission happens we are NOT sending a url with parameters, opting to send json state object instead
+    event.preventDefault()
+    //set newUser to state
+    const joinTrip = this.state.info
+    //send json version of newUser to backend api with post method
+    fetch(`${this.state.apiUrl}/join_trip`,
+      {
+        body: JSON.stringify(joinTrip),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: "POST"
+      }
+    )
+    .then((rawResponse) => { //process response
+      return Promise.all([rawResponse.status, rawResponse.json()])
+    })
+    .then((parsedResponse) =>{ //if response is error, update this.state.error
+      if (parsedResponse[0] === 422) {
+        this.setState({errors: 'Invalid Inputs'})
+      } else { //otherwise redirect to login and (temporarily) set alert=success
+        //todo redirect to login
+        this.setState({errors: null})
+        //TODO redirect to trips dashboard
+        alert('Success')
+      }})
   }
 
   render(){
     return(
-      <Form>
+      <Form onSubmit={this.joinTripSubmit.bind(this)}>
         <Row>
           <div className="form-group">
             <label className="col-form-label col-form-label-lg title">Enter Your Trip Code: </label>
-            <input className="form-control form-control-lg title" type="text" placeholder="Code" name="code" value={this.state.code} onChange={this.handleChange.bind(this)} id="inputLarge" />
+            <input className="form-control form-control-lg title" type="text" placeholder="Code" name="code" value={this.state.info.code} onChange={this.handleChange.bind(this)} id="inputLarge" />
           </div>
         </Row>
         <Row>
