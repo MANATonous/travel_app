1c864d5ab75044a6f9eef6e6eac079c1d2f5ee38
Mike Larocca <michaellarocca90@gmail.com>
2018-04-03 16:51:25 -0700

    can now load trips by user_id, needs to also render trips that user has been invited to, and needs to render images properly

--- travel_app_backend/config/routes.rb
+++ travel_app_backend/config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   resources :trips
   resources :users
   resources :register
+  get 'trips_by_user/:user_id' => 'trips#trips_by_user'
   post 'user_token' => 'user_token#create'
   post 'join_trip' => 'trips#join'
   post 'find_trip' => 'trips#find_trip'
+  post 'trips_by_user' => 'trips#trips_by_user'
   get '/messages', to: 'messages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
