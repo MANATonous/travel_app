1321d51625f9467b4c3e0efc6b842bb2c21a6545
michaellarocca90 <33819929+michaellarocca90@users.noreply.github.com>
2018-04-04 10:30:29 -0700

    Update trips_controller.rb

--- travel_app_backend/app/controllers/trips_controller.rb
+++ travel_app_backend/app/controllers/trips_controller.rb
@@ -4,7 +4,6 @@ class TripsController < ApplicationController
     @trips_by_user = Trip.where("user_id = #{params[:user_id]}")
   end
 
-
   def find_trip
     #take in id from post
     #find the id
